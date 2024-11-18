1e68c1d1e5e6edd00f23aa41cb138180416c54e7
Jiří Malec <jiri.malec@bluesoft.cz>
2024-11-18 12:44:50 +0100

    Adding more triggers to seed db

--- scripts/Seed-Database.ps1
+++ scripts/Seed-Database.ps1
@@ -21,9 +21,10 @@ $connection = Get-ConnectionString $appSettings
 
 $command = "Invoke-SqlQuery " + `
     "-connectionString ""$connection"" " + `
-    "-query ""INSERT INTO KenticoZapier_ZapierTrigger  
-        VALUES  
-        ('e569a1c7', 'BizForm.DancingGoatContactUs', 'Form', 'Create', 'https://hooks.zapier.com/hooks/standard/18364481/7669f3b896c3454da513d8f361464208/');
+    "-query ""INSERT INTO KenticoZapier_ZapierTrigger VALUES
+    ('e569a1c7', 'BizForm.DancingGoatContactUs', 'Form', 'Create', 'https://hooks.zapier.com/hooks/standard/18364481/7669f3b896c3454da513d8f361464208/'),
+    ('e569a1c8', 'DancingGoat.Cafe', 'Reusable', 'Update', 'https://hooks.zapier.com/hooks/standard/18364481/7669f3b896c3454da513d8f361464209/'),
+    ('e569a1c9', 'CMS.EventLog', '', 'Create', 'https://hooks.zapier.com/hooks/standard/18364481/7669f3b896c3454da513d8f361464207/');
     "" "
 
 Invoke-ExpressionWithException $command
