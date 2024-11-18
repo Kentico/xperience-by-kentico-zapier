23487853f54f61fb584c6d36953a9537cb70ee0f
Jiří Malec <jiri.malec@bluesoft.cz>
2024-11-18 12:32:11 +0100

    seeding db for testing purposes

---
+++ scripts/Seed-Database.ps1
@@ -0,0 +1,33 @@
+<#
+.Synopsis
+    Toggle CMSEnableCI Key valu in DB with flag.
+#>
+
+Import-Module (Resolve-Path Settings) `
+    -Function `
+    Get-AppSettings `
+    -Force
+
+Import-Module (Resolve-Path Utilities) `
+    -Function `
+    Invoke-ExpressionWithException, `
+    Invoke-SqlQuery, `
+    Get-ConnectionString, `
+    Write-Status `
+    -Force
+
+$appSettings = Get-AppSettings
+$connection = Get-ConnectionString $appSettings
+
+$command = "Invoke-SqlQuery " + `
+    "-connectionString ""$connection"" " + `
+    "-query ""INSERT INTO KenticoZapier_ZapierTrigger  
+        VALUES  
+        ('e569a1c7', 'BizForm.DancingGoatContactUs', 'Form', 'Create', 'https://hooks.zapier.com/hooks/standard/18364481/7669f3b896c3454da513d8f361464208/');
+    "" "
+
+Invoke-ExpressionWithException $command
+
+Write-Host "`n"
+Write-Status "Database seeded"
+Write-Host "`n"
